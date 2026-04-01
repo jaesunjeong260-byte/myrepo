fb8b18be8a9b7d1bba9f8337a9c7594dfaa87693
Jeong Jaesun <jaesunjeong260@gmail.com>
2026-04-01 18:34:21 +0900

    feat: add python script

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,5 +1,8 @@
 import datetime
+import os
 
 now = datetime.datetime.now()
-print(f"파이썬 실행 시간: {now}")
-print("자동화가 정상적으로 작동 중입니다!")
+print(f"--- 파이썬 자동화 실행 로그 ---")
+print(f"실행 시간: {now}")
+print(f"실행 환경: {os.name}")
+print(f"------------------------------")
